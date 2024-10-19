aea16e623ca02bea8f2d00dea8c8ea8faa36ed96
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-10-19 07:05:06 +0530

    routes are  implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import Login from "./pages/Login";
+
 export default function App() {
+  const token = false;
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+       <Route path="*" element={token ? <UserPage /> : <Login/> } />
+     </Routes>
   )
 }
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserPage() {
+  return (
+    <div>
+      userPage
+    </div>
+  )
+}
+
+export default UserPage
